e329a11119f0d204df24ffdf60e8a5cb66d05294
yeonupark <idepix@naver.com>
2023-07-25 15:47:32 +0900

    Initial Commit

---
+++ EmotionController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmotionController
+//
+//  Created by 마르 on 2023/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
